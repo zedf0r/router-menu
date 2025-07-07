6bec4d0287a7467895bc8bd60395a05a8efbf3bf
Leonid <zemlyanykh@bk.ru>
2025-07-07 13:36:09 +0500

    Added Outlet

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import { Route, Routes } from "react-router-dom";
 import { Layout } from "./components/Layout";
+import {
+  DriftPage,
+  TimeAttackPage,
+  ForzaPage,
+  HomePage,
+} from "./components/pages";
 
 function App() {
-  return <Layout />;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/drift" element={<DriftPage />} />
+          <Route path="/timeattack" element={<TimeAttackPage />} />
+          <Route path="/forza" element={<ForzaPage />} />
+        </Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,18 +1,12 @@
-import { Route, Routes } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { Menu } from "./Menu";
-import { HomePage, DriftPage, TimeAttackPage, ForzaPage } from "./pages/routes";
 
 export const Layout = () => {
   return (
     <>
       <Menu />
       <div className="page">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/drift" element={<DriftPage />} />
-          <Route path="/timeattack" element={<TimeAttackPage />} />
-          <Route path="/forza" element={<ForzaPage />} />
-        </Routes>
+        <Outlet />
       </div>
     </>
   );
